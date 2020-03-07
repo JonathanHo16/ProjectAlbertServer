257663627e8f722d37b285f3423e07ff246b24ee
Jonathan Ho <jonathanbho@outlook.com>
2020-03-07 18:08:45 -0500

    added some cors nonsense

--- go.mod
+++ go.mod
@@ -4,13 +4,15 @@ go 1.13
 
 require (
 	cloud.google.com/go/firestore v1.1.1
-	cloud.google.com/go/storage v1.6.0
+	cloud.google.com/go/storage v1.6.0 // indirect
 	github.com/99designs/gqlgen v0.11.1
+	github.com/go-chi/chi v4.0.3+incompatible
 	github.com/golang/protobuf v1.3.4 // indirect
 	github.com/google/uuid v1.1.1 // indirect
-	github.com/graphql-go/graphql v0.7.9
+	github.com/gorilla/websocket v1.2.0
+	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser/v2 v2.0.1
-	golang.org/x/exp v0.0.0-20200228211341-fcea875c7e85
+	golang.org/x/exp v0.0.0-20200228211341-fcea875c7e85 // indirect
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
 	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
--- server.go
+++ server.go
@@ -1,28 +1,52 @@
 package main
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
 	"github.com/jonathanho/gqlgen/graph"
 	"github.com/jonathanho/gqlgen/graph/generated"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
+
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				return r.Host == "localhost:3000"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
-
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
