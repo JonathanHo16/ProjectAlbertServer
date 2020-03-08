9a8d01012969dd859b693dd9b546831308430fa5
Jonathan Ho <jonathanbho@outlook.com>
2020-03-07 23:28:40 -0500

    fixing some permissions

--- server.go
+++ server.go
@@ -22,12 +22,8 @@ func main() {
 	router := chi.NewRouter()
 
 	// Add CORS middleware around every request
-	// See for full option listing
-	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
-		AllowCredentials: true,
-		Debug:            true,
-	}).Handler)
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.Default().Handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -45,8 +41,8 @@ func main() {
 			WriteBufferSize: 1024,
 		},
 	})
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
